cec26130f53cd79ca47d1158883e2a922d00b439
Lucas Andrade <Lucas Andrade>
2021-06-20 11:51:14 -0300

    add more components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/63925588?v=4" alt="Lucas Andrade" />
+            <div>
+                <strong>Lucas Andrade</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+} 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
-import Head from 'next/head'
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+import { CompletedChallegens } from '../components/CompletedChallegens'
+import { Countdown } from '../components/Countdown'
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home(){
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallegens />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
-  )
+  );
 }
